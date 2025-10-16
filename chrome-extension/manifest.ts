0f6c00ee581574e3340a8fe0556ac4379bba8066
Daesung Ha <daesung@amazon.com>
2025-10-16 01:03:52 -0700

    remove unnecessary permissions

--- chrome-extension/manifest.ts
+++ chrome-extension/manifest.ts
@@ -22,16 +22,10 @@ const manifest = {
   manifest_version: 3,
   default_locale: 'en',
   name: '__MSG_extensionName__',
-  browser_specific_settings: {
-    gecko: {
-      id: 'example@example.com',
-      strict_min_version: '109.0',
-    },
-  },
   version: packageJson.version,
   description: '__MSG_extensionDescription__',
   host_permissions: ['<all_urls>'],
-  permissions: ['storage', 'scripting', 'tabs', 'notifications', 'sidePanel'],
+  permissions: ['storage', 'tabs', 'sidePanel'],
   options_page: 'options/index.html',
   background: {
     service_worker: 'background.js',
