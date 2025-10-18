3da6de7d93302895e6406cc1da67c9175b19c616
Daesung Ha <daesung@amazon.com>
2025-10-17 21:19:44 -0700

    fix all lint errors

--- pages/side-panel/src/SidePanel.tsx
+++ pages/side-panel/src/SidePanel.tsx
@@ -1,19 +1,19 @@
 import '@src/SidePanel.css';
+import CabinetInfo from './CabinetInfo';
+import TabList from './TabList';
 import { useStorage, withErrorBoundary, withSuspense } from '@extension/shared';
 import { exampleThemeStorage } from '@extension/storage';
 import { cn, ErrorDisplay, LoadingSpinner } from '@extension/ui';
-import CabinetInfo from './CabinetInfo';
-import TabList from './TabList';
 
 const SidePanel = () => {
-    const { isLight } = useStorage(exampleThemeStorage);
+  const { isLight } = useStorage(exampleThemeStorage);
 
-    return (
-        <div className={cn('App', isLight ? 'bg-slate-50' : 'bg-gray-800', 'min-h-screen')}>
-            <CabinetInfo />
-            <TabList />
-        </div>
-    );
+  return (
+    <div className={cn('App', isLight ? 'bg-slate-50' : 'bg-gray-800', 'min-h-screen')}>
+      <CabinetInfo />
+      <TabList />
+    </div>
+  );
 };
 
 export default withErrorBoundary(withSuspense(SidePanel, <LoadingSpinner />), ErrorDisplay);
